c8bb31445104b2f0d91eb6c5a6a9d3c05fd5c50f
AustineGwa <austinegwa64@gmail.com>
2019-02-16 12:47:43 +0300

    added  firebase authentication to app

--- app/src/main/java/com/gwazasoftwares/DscDemo/SignUp.java
+++ app/src/main/java/com/gwazasoftwares/DscDemo/SignUp.java
@@ -3,8 +3,10 @@ package com.gwazasoftwares.DscDemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -21,6 +29,8 @@ public class SignUp extends Fragment {
     EditText username, email, password,confirmpassword;
     Button create;
     TextView terms;
+
+    private FirebaseAuth mAuth;
 
 
     public SignUp() {
@@ -38,6 +48,8 @@ public class SignUp extends Fragment {
         password = view.findViewById(R.id.createpassword);
         confirmpassword = view.findViewById(R.id.confirmpassword);
 
+        mAuth = FirebaseAuth.getInstance();
+
         create = view.findViewById(R.id.createaccount);
         terms = view.findViewById(R.id.terms);
         return view;
@@ -46,6 +58,10 @@ public class SignUp extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,15 +77,43 @@ public class SignUp extends Fragment {
                 }else if(!mPassword.equals(mConfirmPassword)){
                     Toast.makeText(getActivity(),"passwords  do not match", Toast.LENGTH_SHORT).show();
                 }else {
-                    createAcount(mUsername, mEmail, mPassword);
+                    createAcount(mEmail, mPassword);
                 }
             }
         });
     }
 
-    private void createAcount(String mUsername, String mEmail, String mPassword) {
 
+
+
+    private void createAcount( String mEmail, String mPassword) {
+      //start of mAutH
+
+
+        mAuth.createUserWithEmailAndPassword(mEmail, mPassword)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                           Toast.makeText(getActivity(), "User created successfully", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                           //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Activity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "createUserWithEmail:failure",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+
+                        }
+
+                        // ...
+                    }
+                });
     }
+
+
 
 
     private void login() {
